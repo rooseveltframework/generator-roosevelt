0cbfcf05bde755a0e3330814e0f937aaeff4c143
Eric Newport <kethinov@gmail.com>
2014-05-23 01:33:35 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
-mkroosevelt
-===========
+mkroosevelt.js [![NPM version](https://badge.fury.io/js/mkroosevelt.png)](http://badge.fury.io/js/roosevelt) [![Dependency Status](https://gemnasium.com/kethinov/mkroosevelt.png)](https://gemnasium.com/kethinov/roosevelt) [![Gittip](http://img.shields.io/gittip/kethinov.png)](https://www.gittip.com/kethinov/)
+===
 
-Command line application for creating Roosevelt apps.
+Command line application for creating [Roosevelt](https://github.com/kethinov/roosevelt) apps.
+
+![Teddy Roosevelt's facial hair is a curly brace.](https://raw.github.com/kethinov/mkroosevelt/master/sampleApp/statics/images/teddy.jpg "Teddy Roosevelt's facial hair is a curly brace.")
+
+License
+===
+
+All original code in mkroosevelt is licensed under the [Creative Commons Attribution 4.0 International License](http://creativecommons.org/licenses/by/4.0/). Commercial and noncommercial use is permitted with attribution.
---
+++ mkroosevelt.js
@@ -0,0 +1,49 @@
+#! /usr/bin/env node
+var fs = require('fs'),
+    path = require('path'),
+    package = require('./package.json'),
+    wrench = require('wrench'),
+    updateNotifier = require('update-notifier'),
+    notifier = updateNotifier(),
+    cmd = process.argv[2],
+    arg = process.argv[3],
+    showHelp = function() {
+      console.log("");
+      console.log("USAGE:");
+      console.log("");
+      console.log("create an app in this directory:");
+      console.log("mkroosevelt create appName");
+      console.log("");
+      console.log("create an app somewhere else:");
+      console.log("mkroosevelt create /path/to/appName");
+    };
+
+if (notifier.update) {
+  notifier.notify();
+}
+
+if (cmd && arg) {
+  if (cmd === 'create') {
+    try {
+      wrench.copyDirSyncRecursive(path.normalize(__dirname + '/sampleApp/'), path.normalize(arg), {
+        forceDelete: false, // Whether to overwrite existing directory or not
+        excludeHiddenUnix: false, // Whether to copy hidden Unix files or not (preceding .)
+        preserveFiles: true, // If we're overwriting something and the file already exists, keep the existing
+        preserveTimestamps: false, // Preserve the mtime and atime when copying files
+        inflateSymlinks: false // Whether to follow symlinks or not when copying files
+      });
+      if (fs.existsSync(path.normalize(arg + '/.npmignore'))) {
+        fs.renameSync(path.normalize(arg + '/.npmignore'), path.normalize(arg + '/.gitignore')); // fix to compensate for this "feature" https://github.com/npm/npm/issues/1862
+      }
+    }
+    catch (e) {
+      console.error(e);
+    }
+  }
+}
+else if (cmd && (cmd === '-v' || cmd === '--v' || cmd === '-version' || cmd === '--version')) {
+  console.log(package.version);
+}
+else {
+  showHelp();
+}
