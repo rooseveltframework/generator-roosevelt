b23570bd00555112ec0dfc26e321656b189a0284
Eric Newport <kethinov@gmail.com>
2014-08-03 00:44:46 -0700

    fixing express warning

--- sampleApp/mvc/controllers/404.js
+++ sampleApp/mvc/controllers/404.js
@@ -2,7 +2,7 @@ module.exports = function(app) {
   app.route('*').all(function(req, res) {
     var model = require('models/global')(req, res);
     model.content.pageTitle = '{content.appTitle} - 404 not found';
-    model.host = req.host;
+    model.host = req.hostname;
     model.url = req.url;
     model.appVersion = app.get('package').version;
     res.status(404);
