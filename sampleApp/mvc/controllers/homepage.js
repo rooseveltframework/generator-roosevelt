185f96edc6565cdb847787806de360cdb04df368
Eric Newport <kethinov@gmail.com>
2015-03-12 12:15:39 -0400

    fix for 404 title persist bug in sample app

--- sampleApp/mvc/controllers/homepage.js
+++ sampleApp/mvc/controllers/homepage.js
@@ -1,6 +1,7 @@
 module.exports = function(app) {
   app.route('/').get(function(req, res) {
     var model = require('models/global')(req, res);
+    model.content.pageTitle = '{content.appTitle} - Homepage';
     model.content.hello = 'Hi! I\'m a variable trickling down through the MVC structure!';
     model.content.picLabel = 'Here\'s a silly picture of Teddy Roosevelt:';
     model.teddyPath = '/images/teddy.jpg';
