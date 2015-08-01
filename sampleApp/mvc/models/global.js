7b0bd8b8237e76c468cbd61f3fc0a7dc42ccc19b
Eric Newport <kethinov@gmail.com>
2015-07-31 23:34:08 -0400

    add node_env to default model

--- sampleApp/mvc/models/global.js
+++ sampleApp/mvc/models/global.js
@@ -12,6 +12,7 @@ module.exports = function(req, res) {
   // recalculated each require
   model.currentYear = new Date().getFullYear();
   model.mainDomain = req.headers['x-forwarded-host'] || req.headers.host;
+  model.NODE_ENV = process.env.NODE_ENV;
 
   return model;
 };
