4c2efe9da249d04549c365c05f222cc9daa6125d
Eric Newport <kethinov@gmail.com>
2015-08-13 13:43:43 -0400

    closes #10

--- sampleApp/mvc/models/global.js
+++ sampleApp/mvc/models/global.js
@@ -8,11 +8,14 @@ var model = {
 
 // extend global model provide additional useful vars at runtime and export it
 module.exports = function(req, res) {
+  return {
 
-  // recalculated each require
-  model.currentYear = new Date().getFullYear();
-  model.mainDomain = req.headers['x-forwarded-host'] || req.headers.host;
-  model.NODE_ENV = process.env.NODE_ENV;
-
-  return model;
+    // always static
+    content: model.content,
+    
+    // recalculated each require
+    currentYear: new Date().getFullYear(),
+    mainDomain: req.headers['x-forwarded-host'] || req.headers.host,
+    NODE_ENV: process.env.NODE_ENV
+  };
 };
