019db5eef9ee915dc357c205980dbbd5b5d4b9b5
Alexander J, Lallier <alallier@caci.com>
2016-06-08 14:40:21 -0400

    Added eslint, htmlhint, and lesshint to the sample app
    Added eslint, htmlhint, and lesshint to the sample app and removed
    jshint. Also fixed errors that the linters provided.

---
+++ sampleApp/test/lint.js
@@ -0,0 +1,40 @@
+#!/usr/bin/env node
+
+var exec = require('child_process').exec,
+    path = require('path'),
+
+    htmlLinterPath = path.normalize('./node_modules/.bin/htmlhint --config ./test/.htmlhintrc mvc/views'),
+    htmlLinterActualPath = path.join(__dirname, '..', htmlLinterPath),
+
+    lessLinterPath = path.normalize('./node_modules/.bin/lesshint -c ./test/.lesshintrc statics/css'),
+    lessLinterActualPath = path.join(__dirname, '..', lessLinterPath),
+
+    jsLinterPath = path.normalize('./node_modules/.bin/eslint .'),
+    jsLinterActualPath = path.join(__dirname, '..', jsLinterPath);
+
+console.log('Linting HTML...\n');
+exec(htmlLinterActualPath, function(error, stdout, stderr) {
+  console.log(stdout);
+  console.log('Done linting HTML\n');
+  if (error !== null) {
+    console.error(error);
+  }
+  console.log('Linting LESS...\n');
+});
+
+exec(lessLinterActualPath, function(error, stdout, stderr) {
+  console.log(stdout);
+  console.log('Done linting LESS\n');
+  if (error !== null) {
+    console.error(error);
+  }
+  console.log('Linting JS...\n');
+});
+
+exec(jsLinterActualPath, function(error, stdout, stderr) {
+  console.log(stdout);
+  console.log('Done linting JS\n');
+  if (error !== null) {
+    console.error(error);
+  }
+});
