e4bce764b06a501ba4b53a810bde9b0249d7d7f3
Oumar Ly <lyomrau@gmail.com>
2017-06-09 16:39:16 -0400

    Match rooseveltConfigs for sampleApp to the ones made in roosevelt

--- generators/app/index.js
+++ generators/app/index.js
@@ -26,7 +26,7 @@ module.exports = generators.Base.extend({
     this.option('requestCert', {desc: 'Request a certificate from a client and attempt to verify it.' });
     this.option('rejectUnauthorized', {desc: 'Upon failing to authorize a user with supplied CA(s), reject their connection entirely.' });
     this.option('bodyParserOptions', {desc: 'Controls the options for body-parser using a object.' });
-    this.option('bodyParserJsonOptions', {desc: 'Controls the options for the json function of the body-parser using a object.' });
+    this.option('bodyParserJsonParams', {desc: 'Controls the options for the json function of the body-parser using a object.' });
     this.option('modelsPath', {desc: 'Relative path on filesystem to where your model files are located.' });
     this.option('modelsNodeModulesSymlink', {desc: 'Name of the symlink to make in node_modules pointing to your models directory. Set to false to disable making this symlink.' });
     this.option('viewsPath', {desc: 'Relative path on filesystem to where your view files are located.' });
@@ -312,7 +312,7 @@ module.exports = generators.Base.extend({
           this.requestCert = answers.requestCert ?  answers.requestCert :  this.options.requestCert || 'false';
           this.rejectUnauthorized = answers.rejectUnauthorized ? answers.rejectUnauthorized :  this.options.rejectUnauthorized || 'false';
           this.bodyParserOptions = answers.bodyParserOptions ? answers.bodyParserOptions :  this.options.bodyParserOptions || '{"extended": true}';
-          this.bodyParserJsonOptions = answers.bodyParserJsonOptions ? answers.bodyParserJsonOptions :  this.options.bodyParserJsonOptions || '{}';
+          this.bodyParserJsonParams = answers.bodyParserJsonParams ? answers.bodyParserJsonParams :  this.options.bodyParserJsonParams || '{}';
           this.modelsPath = answers.modelsPath ? answers.modelsPath :  this.options.modelsPath || 'mvc/models';
           this.modelsNodeModulesSymlink = answers.modelsNodeModulesSymlink ? answers.modelsNodeModulesSymlink :  this.options.modelsNodeModulesSymlink || 'models';
           this.viewsPath = answers.viewsPath ? answers.viewsPath :  this.options.viewsPath || 'mvc/views';
@@ -596,7 +596,7 @@ module.exports = generators.Base.extend({
         requestCert: this.requestCert,
         rejectUnauthorized: this.rejectUnauthorized,
         bodyParserOptions: this.bodyParserOptions,
-        bodyParserJsonOptions: this.bodyParserJsonOptions,
+        bodyParserJsonParams: this.bodyParserJsonParams,
         modelsPath: this.modelsPath,
         modelsNodeModulesSymlink: this.modelsNodeModulesSymlink,
         viewsPath: this.viewsPath,
