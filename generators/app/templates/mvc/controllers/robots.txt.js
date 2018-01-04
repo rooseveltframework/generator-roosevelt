3e780748029cc68e0a43fedcdb868800a1f685c3
Troy Coutu <Autre31415@gmail.com>
2018-01-04 18:59:40 -0500

    Update generator for Roosevelt 0.11.x

--- generators/app/templates/mvc/controllers/robots.txt.js
+++ generators/app/templates/mvc/controllers/robots.txt.js
@@ -1,15 +1,8 @@
-const fs = require('fs')
+const path = require('path')
 
 module.exports = function (app) {
   app.route('/robots.txt').get(function (req, res) {
     res.setHeader('Content-Type', 'text/plain')
-
-    // it's plain text, so we don't want to render it with the template parser
-    fs.readFile(app.get('viewsPath') + 'robots.txt', 'utf8', function (err, data) {
-      if (err) {
-        console.error(err)
-      }
-      res.send(data)
-    })
+    res.sendFile(path.join(app.get('viewsPath'), 'robots.txt'))
   })
 }
