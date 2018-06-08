2fd169caa71fe3db9d5bfbfc50274027cca4acba
Michael Mulligan <mkmllgn@gmail.com>
2018-06-08 14:47:56 -0400

    Unit Testing (In Progress) (#132)
    * Prompt Unit Tests 1
    
    Unit tests to test user prompting with file generation.
    
    * Unit Tests (In Progress)
    
    Unit tests for many of the prompts.
    
    * Rename of HTTPS port number variable
    
    Change to the HTTPS Port Number variable because it was the same as the standard HTTP Port Number variable.
    
    * Fix for Travis CI
    
    Fix for Travis CI so the build passes.
    
    * Added more unit tests
    
    Added more unit tests for user prompts.
    
    * Updated package-lock
    
    Updated package-lock to fix travis ci.
    
    * Fix for function timeout
    
    Function timeout  fixed for build checks.
    
    * Refactor input validation
    
    This refactors input validation to store reusable validation functions into an object vs using anonymous functions like before.
    
    * Unit tests for helper functions
    
    Unit tests that test the helper functions.
    
    * More refactoring
    
    This commit creates one more helper function and refactors the view engine recursion to a do..while to pass unit tests.
    
    * Revert to prior chooseViewEngine()
    
    Revert back to prior chooseViewEngine() function and new unit test for it.
    
    * Revert "Revert to prior chooseViewEngine()"
    
    This reverts commit 1bcfff80420781e19fcc87cdb5165832e898e9fb.
    
    * Removed failed unit test
    
    * Updated chooseViewEngine() for unit tests.
    
    This allows us to increment the prompt names when recursively calling the function so we can unit test this properly using yeoman-test.
    
    * Updated package-lock for merge
    
    * Refactored Unit Tests
    
    Unit test refactoring to remove unnecessary tests while keeping coverage at 100%.
    
    * Removed Extra Defaults
    
    removed extra default prompts that aren't needed.
    
    * Set AppVeyor node version to 10.1
    
    This is because of the unit tests breaking when using yeoman-test with AppVeyor and Node 10.2 and higher. 10.1 is the latest stable version of Node that yeoman-test will work with AppVeyor.

---
+++ test/unit/prompts.js
@@ -0,0 +1,247 @@
+/* eslint-env mocha */
+
+/**
+ * Unit tests for prompts
+ * @module test/unit/prompts
+ */
+
+const path = require('path')
+const assert = require('yeoman-assert')
+const helpers = require('yeoman-test')
+const fs = require('fs')
+
+describe('Generator Prompts', function () {
+  describe('Templating Engine', function () {
+    it('Should use multiple view engines and templating extensions', function () {
+      return helpers.run(path.join(__dirname, '../../generators/app'))
+        .withPrompts({
+          configMode: 'Customize',
+          templatingEngine: true,
+          templatingEngineName1: 'test1',
+          templatingExtension1: 'html1',
+          additionalTemplatingEngines1: true,
+          templatingEngineName2: 'test2',
+          templatingExtension2: 'html2',
+          additionalTemplatingEngines2: true,
+          templatingEngineName3: 'test3',
+          templatingExtension3: 'html3',
+          additionalTemplatingEngines3: false
+        })
+        .then(function () {
+          assert.JSONFileContent('package.json', {
+            rooseveltConfig: {
+              viewEngine: [`html1: test1`, 'html2: test2', 'html3: test3']
+            }
+          })
+        })
+    })
+  })
+
+  describe('HTTP Ports', function () {
+    it('Should set the HTTP Port to a custom port', function () {
+      return helpers.run(path.join(__dirname, '../../generators/app'))
+        .withPrompts({
+          configMode: 'Customize',
+          portNumber: 'Custom',
+          customHttpPort: 1234
+        })
+        .then(function () {
+          assert.JSONFileContent('package.json', {
+            rooseveltConfig: {
+              port: 1234
+            }
+          })
+        })
+    })
+
+    it('Should set the HTTP Port to a random port', function () {
+      return helpers.run(path.join(__dirname, '../../generators/app'))
+        .withPrompts({
+          configMode: 'Customize',
+          portNumber: 'Random'
+        })
+        .then(function () {
+          var data = fs.readFileSync('package.json')
+          var jsonData = JSON.parse(data)
+          assert.strictEqual(typeof jsonData.rooseveltConfig.https.httpsPort, 'number')
+        })
+    })
+  })
+
+  describe('HTTPS Ports', function () {
+    it('Should set the HTTPS Port to a custom port', function () {
+      return helpers.run(path.join(__dirname, '../../generators/app'))
+        .withPrompts({
+          configMode: 'Customize',
+          enableHTTPS: true,
+          httpsPortNumber: 'Custom',
+          customHttpsPort: 1234
+        })
+        .then(function () {
+          assert.JSONFileContent('package.json', {
+            rooseveltConfig: {
+              https: {
+                httpsPort: 1234
+              }
+            }
+          })
+        })
+    })
+
+    it('Should set the HTTPS Port to a random port', function () {
+      return helpers.run(path.join(__dirname, '../../generators/app'))
+        .withPrompts({
+          configMode: 'Customize',
+          enableHTTPS: true,
+          httpsPortNumber: 'Random'
+        })
+        .then(function () {
+          var data = fs.readFileSync('package.json')
+          var jsonData = JSON.parse(data)
+          assert.notEqual(jsonData.rooseveltConfig.https.httpsPort, jsonData.rooseveltConfig.port)
+          assert.strictEqual(typeof jsonData.rooseveltConfig.https.httpsPort, 'number')
+        })
+    })
+  })
+
+  describe('SSL', function () {
+    it('Should create SSL certs if generate ssl is true', function () {
+      this.timeout(10000)
+      return helpers.run(path.join(__dirname, '../../generators/app'))
+        .withPrompts({
+          configMode: 'Customize',
+          enableHTTPS: true,
+          generateSSL: true,
+          commonName: 'www.google.com',
+          countryName: 'US',
+          pfx: '.pfx'
+        })
+        .then(function (done) {
+          assert.file('certPem.pem')
+          assert.file('privatePem.pem')
+          assert.file('public.pem')
+        })
+    })
+  })
+
+  describe('Statics', function () {
+    it('Should set the CSS preprocessor to LESS', function () {
+      return helpers.run(path.join(__dirname, '../../generators/app'))
+        .withPrompts({
+          configMode: 'Customize',
+          cssCompiler: 'LESS'
+        })
+        .then(function () {
+          assert.JSONFileContent('package.json', {
+            rooseveltConfig: {
+              css: {
+                compiler: {
+                  nodeModule: 'roosevelt-less'
+                }
+              }
+            }
+          })
+          assert.file('statics/css/more.less')
+          assert.file('statics/css/styles.less')
+        })
+    })
+
+    it('Should set the CSS preprocessor to SASS', function () {
+      return helpers.run(path.join(__dirname, '../../generators/app'))
+        .withPrompts({
+          configMode: 'Customize',
+          cssCompiler: 'SASS'
+        })
+        .then(function () {
+          assert.JSONFileContent('package.json', {
+            rooseveltConfig: {
+              css: {
+                compiler: {
+                  nodeModule: 'roosevelt-sass'
+                }
+              }
+            }
+          })
+          assert.file('statics/css/more.scss')
+          assert.file('statics/css/styles.scss')
+        })
+    })
+
+    it('Should set the CSS preprocessor to none', function () {
+      return helpers.run(path.join(__dirname, '../../generators/app'))
+        .withPrompts({
+          configMode: 'Customize',
+          cssCompiler: 'none'
+        })
+        .then(function () {
+          assert.JSONFileContent('package.json', {
+            rooseveltConfig: {
+              css: {
+                compiler: 'none'
+              }
+            }
+          })
+          assert.file('statics/css/styles.css')
+        })
+    })
+
+    it('Should set the JS compiler to UglifyJS', function () {
+      return helpers.run(path.join(__dirname, '../../generators/app'))
+        .withPrompts({
+          configMode: 'Customize',
+          jsCompiler: 'UglifyJS'
+        })
+        .then(function () {
+          assert.JSONFileContent('package.json', {
+            rooseveltConfig: {
+              js: {
+                compiler: {
+                  nodeModule: 'roosevelt-uglify',
+                  params: {}
+                }
+              }
+            }
+          })
+        })
+    })
+
+    it('Should set the JS compiler to Closure', function () {
+      return helpers.run(path.join(__dirname, '../../generators/app'))
+        .withPrompts({
+          configMode: 'Customize',
+          jsCompiler: 'Closure Compiler'
+        })
+        .then(function () {
+          assert.JSONFileContent('package.json', {
+            rooseveltConfig: {
+              js: {
+                compiler: {
+                  nodeModule: 'roosevelt-closure',
+                  params: {
+                    compilationLevel: 'ADVANCED'
+                  }
+                }
+              }
+            }
+          })
+        })
+    })
+
+    it('Should set the JS compiler to none', function () {
+      return helpers.run(path.join(__dirname, '../../generators/app'))
+        .withPrompts({
+          configMode: 'Customize',
+          jsCompiler: 'none'
+        })
+        .then(function () {
+          assert.JSONFileContent('package.json', {
+            rooseveltConfig: {
+              js: {
+                compiler: 'none'
+              }
+            }
+          })
+        })
+    })
+  })
+})
