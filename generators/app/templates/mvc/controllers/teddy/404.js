cb4e146a853ea42ecb2f7b12177cca6a0aeb437b
Oumar Ly <lyomrau@gmail.com>
2019-07-11 11:16:15 -0400

    Update dependency standard to v13

--- generators/app/templates/mvc/controllers/teddy/404.js
+++ generators/app/templates/mvc/controllers/teddy/404.js
@@ -1,6 +1,6 @@
 module.exports = (router, app) => {
   router.route('*').all((req, res) => {
-    let model = require('models/global')(req, res)
+    const model = require('models/global')(req, res)
     model.content.pageTitle = '{content.appTitle} - 404 not found'
     model.host = req.hostname
     model.url = req.url
