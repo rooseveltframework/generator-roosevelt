2a0df7cc92c0271f0306319ed660422750ce5c5b
Oumar Ly <lyomrau@gmail.com>
2019-08-19 18:01:22 -0400

    Update dependency standard to v14

--- generators/app/templates/mvc/controllers/teddy/homepage.js
+++ generators/app/templates/mvc/controllers/teddy/homepage.js
@@ -1,6 +1,6 @@
 module.exports = (router, app) => {
   router.route('/').get((req, res) => {
-    let model = require('models/global')(req, res)
+    const model = require('models/global')(req, res)
     model.content.pageTitle = '{content.appTitle} - Homepage'
     model.content.hello = 'Hi! I\'m a variable trickling down through the MVC structure!'
     model.content.picLabel = 'Here\'s a silly picture of Teddy Roosevelt:'
