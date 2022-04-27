0c0f3fafba13cf5f574f1f37a5aec247f9ee7486
RailonA <railonacosta@gmail.com>
2022-04-27 17:39:56 -0400

    Creat self signed certs

--- generators/app/templates/mvc/models/teddy/homepage.js
+++ generators/app/templates/mvc/models/teddy/homepage.js
@@ -1,5 +1,5 @@
 module.exports = (model) => {
-  model.content.hello = 'Hi! I\'m a variable trickling down through the MVC structure!'
+  model.content.hello = 'HTTPS CHANGES'
   model.content.picLabel = 'Here\'s a silly picture of Teddy Roosevelt:'
   model.teddyPath = '/images/teddy.jpg'
   return model
