e3102260d32e727ba467c12a1831f4e1de20d2ac
RailonA <railonacosta@gmail.com>
2022-04-29 11:05:34 -0400

    Remove any changes made to the homepage.js

--- generators/app/templates/mvc/models/teddy/homepage.js
+++ generators/app/templates/mvc/models/teddy/homepage.js
@@ -1,5 +1,4 @@
 module.exports = (model) => {
-  model.content.hello = 'HTTPS CHANGES'
   model.content.picLabel = 'Here\'s a silly picture of Teddy Roosevelt:'
   model.teddyPath = '/images/teddy.jpg'
   return model
