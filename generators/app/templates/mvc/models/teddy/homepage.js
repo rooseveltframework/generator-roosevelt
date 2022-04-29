54c1c023f91870ec9faf58eda1c16c785b36ce47
RailonA <railonacosta@gmail.com>
2022-04-29 11:13:47 -0400

    Fixed homePage to original content

--- generators/app/templates/mvc/models/teddy/homepage.js
+++ generators/app/templates/mvc/models/teddy/homepage.js
@@ -1,4 +1,5 @@
 module.exports = (model) => {
+  model.content.hello = 'Hi! I\'m a variable trickling down through the MVC structure!'
   model.content.picLabel = 'Here\'s a silly picture of Teddy Roosevelt:'
   model.teddyPath = '/images/teddy.jpg'
   return model
