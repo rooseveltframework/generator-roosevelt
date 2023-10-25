dc40c2581c44b3a33ea1d6df5b2734645a3eb759
Eric Newport <kethinov@gmail.com>
2023-10-25 13:16:09 -0400

    0.21.10
    - Altered default global model to prevent a common race condition.
    - Various dependencies updated.

--- generators/app/templates/mvc/models/teddy/global.js
+++ generators/app/templates/mvc/models/teddy/global.js
@@ -1,13 +1,9 @@
-const model = {
-  content: {
-    appTitle: '<%= appName %>',
-    pageTitle: '{content.appTitle}', // override this on a per route level
-    titleTag: '{content.appTitle} — {content.pageTitle}'
-  }
-}
-
 module.exports = (req, res) => {
   return {
-    content: model.content
+    content: {
+      appTitle: '<%= appName %>',
+      pageTitle: '{content.appTitle}', // override this on a per route level
+      titleTag: '{content.appTitle} — {content.pageTitle}'
+    }
   }
 }
